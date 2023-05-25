b81d453be3d55f4619908bfd8f1449c95f4ff5f8
PurpxPanda <116929120+PurpxPanda@users.noreply.github.com>
2023-05-25 14:32:21 -0500

    app start and app.js setup

---
+++ client/src/App.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
