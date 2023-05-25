0a315f8aa54c6102bf3063a58b153d714ce62317
PurpxPanda <116929120+PurpxPanda@users.noreply.github.com>
2023-05-25 17:43:02 -0500

    Started file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,3 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
